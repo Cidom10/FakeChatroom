e2d079eeea694c5a0aa6807ebe028885a6aeb370
Calvin Idom <idomcal9@gmail.com>
2023-10-16 23:09:16 -0700

    App.tsx issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,18 @@ import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
 import { theme } from '../theme';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Comp, pageProps }: AppProps) {
   return (
     <MantineProvider theme={theme}>
       <Head>
-        <title>Mantine Template</title>
+        <title>THE ChatRoom</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
-      <Component {...pageProps} />
+      <Comp {...pageProps} />
     </MantineProvider>
   );
 }
